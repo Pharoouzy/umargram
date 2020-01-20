6b26e3b30c66ff3d3741c31240b4f9f8a67c9578
Pharoouzy <pharoouzy@yahoo.com>
2020-01-20 18:58:05 +0100

    Updated database.php file

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'umardb'),
             'username' => env('DB_USERNAME', 'pharoouzy'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'umarfarouq'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
